27275c0fc485d041fdc32fadfa2bb300ad7b3e4d
zalmes <yurachernov12@gmail.com>
2025-12-08 15:03:14 +0400

    fix: Set channel grid modal to 75vw width

--- src/components/channels/ChannelGridModal.tsx
+++ src/components/channels/ChannelGridModal.tsx
@@ -106,7 +106,7 @@ export function ChannelGridModal({ open, onOpenChange }: ChannelGridModalProps) 
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-[98vw] w-[98vw] h-[95vh] p-0 gap-0 flex flex-col">
+      <DialogContent className="max-w-[75vw] w-[75vw] h-[90vh] p-0 gap-0 flex flex-col">
         <DialogHeader className="px-4 py-3 border-b shrink-0">
           <div className="flex items-center justify-between">
             <DialogTitle className="flex items-center gap-2">
